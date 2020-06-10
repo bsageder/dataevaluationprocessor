7c55e4bc36c0899ea713a70acb1ace7b0a88a7ae
Bernhard Sageder <bernhard@sageder.net>
2020-06-10 09:37:25 +0200

    Changes for step 1

---
+++ dataevaluationprocessor/LogFileProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace dataevaluationprocessor
+{
+    public class LogFileProcessor
+    {
+        public DataTable RunProcess(string fileName)
+        {
+            var splittedLines = new CsvProductionLogFileParser().Parse(fileName);
+            var evaluatedLines = new CsvProductionLogSortByActualPressureProcessor().Process(splittedLines);
+            return new CreateDataTableProcessor().Process(evaluatedLines);
+        }
+    }
+}
--- dataevaluationprocessor/MainForm.cs
+++ dataevaluationprocessor/MainForm.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace dataevaluationprocessor
@@ -19,33 +14,8 @@ namespace dataevaluationprocessor
         {
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                dataGridView.DataSource = LoadFileAndCreateTable(openFileDialog.FileName);
+                dataGridView.DataSource = new LogFileProcessor().RunProcess(openFileDialog.FileName);
             }
-        }
-
-        private DataTable LoadFileAndCreateTable(string fileName)
-        {
-            var csvLines = File.ReadAllLines(fileName, Encoding.Default);
-            var splittedLines = csvLines
-                .Select(x => x.Split(';'))
-                .ToList();
-            return CreateDataTable(splittedLines);
-        }
-
-        private DataTable CreateDataTable(List<string[]> splittedCsvLines)
-        {
-            DataTable table = new DataTable();
-            // Create columns
-            foreach (string columnName in splittedCsvLines[0])
-            {
-                table.Columns.Add(columnName, typeof(string));
-            }
-            // Create rows
-            foreach (var splittedCsvLine in splittedCsvLines.Skip(1))
-            {
-                table.Rows.Add(splittedCsvLine);
-            }
-            return table;
         }
     }
 }
